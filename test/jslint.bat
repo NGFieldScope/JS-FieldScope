ccdda7731af55e892996f712991fcf50f33bf5bd
erussell <erussell@ngs.org>
2008-06-02 22:23:27 +0000

    Fix options Allow testing individual files Print all errors, not just the first

---
+++ test/jslint.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if "%1"=="" (
+    for %%f in (..\js\*.js) do call :_jslint %%f
+) else (
+    call :_jslint ..\js\%1.js
+)
+
+goto end
+
+:_jslint
+    echo -------------------------------------------------------------------------------
+    echo %1 
+    echo.
+    cscript /B jslint.js < %1
+
+:end
