9e44dad1373f6c6b32cb287e4f15e6e725de951e
erussell <erussell@ngs.org>
2008-07-16 23:20:11 +0000

    Extend the resize delay for Safari

--- js/MetaLens.js
+++ js/MetaLens.js
@@ -37,7 +37,7 @@ FieldScope.MetaLens.GDataProvider = function (inMap, inUrl, inService) {
                 // Run this once and then remove the listener, or
                 // you'll end up in an infinite loop on IE6.
                 var infoWindow = this.map.getExtInfoWindow();
-                window.setTimeout(function () { infoWindow.resize(); }, 100);
+                window.setTimeout(function () { infoWindow.resize(); }, 300);
                 thumbnail.onload = null;
               });
           }
