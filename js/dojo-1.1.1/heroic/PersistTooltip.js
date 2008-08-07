cdd52846a8d81ec2d744687a7746fb9b405c682a
erussell <erussell@ngs.org>
2008-08-07 20:00:46 +0000

    Added PersistToolTip to SVN (should have been added as soon as I started using it)

---
+++ js/dojo-1.1.1/heroic/PersistTooltip.js
@@ -0,0 +1,27 @@
+﻿dojo.require("dijit.Tooltip");
+dojo.provide("heroic.PersistTooltip");
+dojo.declare("heroic.PersistTooltip", [dijit.Tooltip], {
+
+  postCreate: function() {
+      if(!dijit._masterTT){
+          dijit._masterTT = new dijit._MasterTooltip();
+      }
+      // should get the connection list & see if another heroic.widget.PersistTooltip
+      // has already made these connections.
+      dijit._masterTT.connect(dijit._masterTT.domNode,'onmouseover',this.ttPersist);
+      dijit._masterTT.connect(dijit._masterTT.domNode,'onmouseout',this.ttFade);
+      this.inherited("postCreate", arguments);
+    },
+
+  ttPersist: function (evt) {
+      // console.log("persist");
+      this.fadeOut.stop();
+      this.fadeIn.play();
+    },
+
+  ttFade: function (evt) {
+      // console.log("fade");
+      this.fadeOut.play();
+    }
+  }
+);
