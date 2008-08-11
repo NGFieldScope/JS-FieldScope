1619f07ee49ffc73eb4cf2d24fccb0065791db5c
erussell <erussell@ngs.org>
2008-08-11 20:12:51 +0000

    Added page for user self-registration

---
+++ NewUser.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="NewUser.aspx.cs" Inherits="NewUser" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head runat="server">
+    <title>New User</title>
+  </head>
+  <body>
+    <form id="form1" runat="server">
+      <asp:ScriptManager ID="ScriptManager2" runat="server" ScriptMode="Release"></asp:ScriptManager>
+      <div>
+	      <table>
+	        <tr>
+	          <td align="left" colspan="2">
+	            New FieldScope User Account:
+	          </td>
+	        </tr>
+	        <tr>
+            <td align="right" style="font-weight:bold">
+              Username:
+            </td>
+            <td>
+              <asp:TextBox id="FieldScope_Username" runat="server" width="225px" />
+            </td>
+  		    </tr>
+  		    <tr>
+            <td align="right" style="font-weight:bold">
+              School/Organization:
+            </td>
+            <td>
+              <asp:TextBox id="FieldScope_Organization" runat="server" width="225px" />
+            </td>
+  		    </tr>
+  		    <tr>
+            <td align="right" style="font-weight:bold">
+              Email Address:
+            </td>
+            <td>
+              <asp:TextBox id="FieldScope_Email" runat="server" width="225px" />
+            </td>
+  		    </tr>
+  		    <tr>
+            <td align="right" style="font-weight:bold">
+              Password:
+            </td>
+            <td>
+              <asp:TextBox id="FieldScope_Password_1" runat="server" width="170px" textmode="Password" />
+            </td>
+  		    </tr>
+  		    <tr>
+            <td align="right" style="font-weight:bold">
+              Re-Type Password:
+            </td>
+            <td>
+              <asp:TextBox id="FieldScope_Password_2" runat="server" width="170px" textmode="Password" />
+            </td>
+  		    </tr>
+		      <tr>
+            <td align="right" colspan="2">
+              <asp:Label id="FieldScope_NewUser_ErrorMessage" runat="server" Visible="False" ForeColor="Red" />
+              &nbsp;
+              <asp:Button id="FieldScope_NewUser_Button" runat="server" text="Register" OnClick="NewUserButton_Click" />
+            </td>
+          </tr>
+  		  </table>
+      </div>
+    </form>
+  </body>
+</html>
---
+++ NewUser.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Web;
+
+public partial class NewUser : System.Web.UI.Page {
+
+    protected void Page_Load (object sender, EventArgs e) { }
+
+    protected void NewUserButton_Click (object sender, EventArgs e) {
+        string username = FieldScope_Username.Text;
+        if (username.Length == 0) {
+            FieldScope_NewUser_ErrorMessage.Text = "Empty Username";
+            FieldScope_NewUser_ErrorMessage.Visible = true;
+            return;
+        } else if (SqlServer.Service.UsernameExists(username)) {
+            FieldScope_NewUser_ErrorMessage.Text = "Username Already Taken";
+            FieldScope_NewUser_ErrorMessage.Visible = true;
+            return;
+        }
+
+        string organization = FieldScope_Organization.Text;
+        if (organization.Length == 0) {
+            FieldScope_NewUser_ErrorMessage.Text = "Please Enter Your School or Organization";
+            FieldScope_NewUser_ErrorMessage.Visible = true;
+            return;
+        }
+
+        string email = FieldScope_Email.Text;
+        if (!Utilities.Regexp.EMAIL.IsMatch(email)) {
+            FieldScope_NewUser_ErrorMessage.Text = "Please Enter A Valid Email Address";
+            FieldScope_NewUser_ErrorMessage.Visible = true;
+            return;
+        }
+        
+        string password1 = FieldScope_Password_1.Text;
+        string password2 = FieldScope_Password_2.Text;
+        if (!password1.Equals(password2)) {
+            FieldScope_NewUser_ErrorMessage.Text = "Passwords Don't Match";
+            FieldScope_NewUser_ErrorMessage.Visible = true;
+            return;
+        } else if (password1.Length == 0) {
+            FieldScope_NewUser_ErrorMessage.Text = "Please Choose A Non-Empty Password";
+            FieldScope_NewUser_ErrorMessage.Visible = true;
+            return;
+        }
+        
+        if (SqlServer.Service.AddUser(username, password1, organization, email) == 1) {
+            string cookie = SqlServer.Service.CheckLogin(username, password1);
+            if (cookie != null) {
+                HttpCookie c = new HttpCookie("FieldScope_Cookie", cookie);
+                c.Expires = DateTime.Now.AddMinutes(60);
+                Response.SetCookie(c);
+                Response.Redirect("Default.aspx");
+            } else {
+                FieldScope_NewUser_ErrorMessage.Text = "Unable To Create Account";
+                FieldScope_NewUser_ErrorMessage.Visible = true;
+            }
+        } else {
+            FieldScope_NewUser_ErrorMessage.Text = "Unknown Error";
+            FieldScope_NewUser_ErrorMessage.Visible = true;
+        }
+    }
+}
